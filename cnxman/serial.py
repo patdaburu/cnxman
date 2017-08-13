e7bf0b870982d01efb8234d254188c3fe53d283e
patdaburu <pat@daburu.net>
2017-08-13 15:06:14 -0500

    Very early, just-barely-working, version of SerialConnection with retry.

--- cnxman/serial.py
+++ cnxman/serial.py
@@ -9,25 +9,37 @@ Let's manage serial port connections!
 """
 
 from cnxman.basics import Connection
-import serial
+from enum import Enum
+from pydispatch import dispatcher
+import serial as pyserial
 from serial import Serial, SerialException
 import threading
 
 class SerialListener(threading.Thread):
-    def __init__(self, serial: serial.Serial):
-        self._serial = serial
-        self._stop_event = threading.Event()
+
+    class Signals(Enum):
+        """
+        These are the used by serial listener objects.
+
+        :seealso:  :py:func:`pydispatch.dispatcher`
+        """
+        DATA_RECEIVED = 'data-received'  # We received some data! Hooray!
+        READ_ERROR = 'read-error'  # We couldn't read from the connection.
+
+    def __init__(self, serial: pyserial.Serial):
+        super().__init__()
+        # Threads of this type run as daemons.
+        self.daemon = True
+        self._serial = serial  # the serial connection we're monitoring
+        self._terminate_event = threading.Event()  # a threading event to tell us when its time to stop
 
     @property
     def serial(self):
         return self._serial
 
-    def listen(self):
-        self.run(self)
-
-    def stop(self):
+    def terminate(self):
         self._serial.close()
-        self._stop_event.set()
+        self._terminate_event.set()
 
     def run(self):
         if not self._serial.is_open:
@@ -35,41 +47,48 @@ class SerialListener(threading.Thread):
                 self._serial.open()
             except:
                 print("couldn't open the serial port.")
-        while not self._stop_event.is_set():
+        while not self._terminate_event.is_set():
             try:
                 data = self._serial.read()
-                print("got some data: ", data)
+                # Notify interested parties that we got something!
+                dispatcher.send(signal=SerialListener.Signals.DATA_RECEIVED, sender=self, data=data)
+                #print("got some data: ", data)
             except:
                 print("an error occurred while we were reading!")
-                self.stop()
-
+                self.terminate()
+                # Let any interested parties know.
+                dispatcher.send(signal=SerialListener.Signals.READ_ERROR, sender=self)
+                # Bail out.
+                return
 
 
 class SerialConnection(Connection):
 
+    class Signals(Enum):
+        """
+        These are the used by serial listener objects.
+
+        :seealso:  :py:func:`pydispatch.dispatcher`
+        """
+        DATA_RECEIVED = 'data-received'  # We received some data! Hooray!
+
     def __init__(self,
                  port: str,
                  baudrate: int=9600,
-                 bytesize: int = serial.EIGHTBITS,
-                 parity: str=serial.PARITY_NONE,
-                 stopbits: int=serial.STOPBITS_ONE,
+                 bytesize: int = pyserial.EIGHTBITS,
+                 parity: str=pyserial.PARITY_NONE,
+                 stopbits: int=pyserial.STOPBITS_ONE,
                  timeout=None):
-        # Make copies of the port parameters.
-        self._port = port
-        self._baudrate = baudrate
-        self._bytesize = bytesize
-        self._parity = parity
-        self._stopbits = stopbits
-        self._timeout = timeout
+        super().__init__()
+        # Make copies of the port parameters so that we may construct serial ports.
+        self._port = port  # To what port are we connecting?
+        self._baudrate = baudrate  # What's the rate on the port?
+        self._bytesize = bytesize  # How much is a byte?
+        self._parity = parity  # What's the parity on the port?
+        self._stopbits = stopbits  # How many stop bits?
+        self._timeout = timeout  # What's the timeout interval.
+        self._listener: SerialListener = None  # the background thread serial monitor
 
-        self._listener = None
-        # Create the internal serial port.
-        # self._serial = serial.Serial(port=port,
-        #                              baudrate=baudrate,
-        #                              bytesize=bytesize,
-        #                              parity=parity,
-        #                              stopbits=stopbits,
-        #                              timeout=timeout)
 
     def try_connect(self) -> bool:
         # If we're already listening and everything is all right...
@@ -77,29 +96,50 @@ class SerialConnection(Connection):
             # ...there's nothing more to do here.
             return True
         try:
-            _serial = serial.Serial(port=self._port,
-                                    baudrate=self._baudrate,
-                                    bytesize=self._bytesize,
-                                    parity=self._parity,
-                                    stopbits=self._stopbits,
-                                    timeout=self._timeout)
-            if not _serial.is_open:
-                _serial.open()
-            self._listener = SerialListener(serial=_serial)
+            serial = Serial(port=self._port,
+                            baudrate=self._baudrate,
+                            bytesize=self._bytesize,
+                            parity=self._parity,
+                            stopbits=self._stopbits,
+                            timeout=self._timeout)
+            # If the serial connection didn't open automatically...
+            if not serial.is_open:
+                # ...open it now.
+                serial.open()
+            # So far so good.  Set up a background thread.
+            self._listener = SerialListener(serial=serial)
+            # We want to be notified if the connection sends data.
+            dispatcher.connect(self._handle_listener_data_received,
+                               signal=SerialListener.Signals.DATA_RECEIVED,
+                               sender=self._listener)
+            # We want to be notified if there is a read error, also.
+            dispatcher.connect(self._handle_listener_read_error,
+                               signal=SerialListener.Signals.READ_ERROR,
+                               sender=self._listener)
             self._listener.run()
-
+            # If we got this far, the connection succeeded.
             return True
         except SerialException as sex:
-            print("boom boom", sex) # TODO: Proper logging!
+            print("boom boom", sex)  # TODO: Proper logging!
             return False
 
     def disconnect(self):
         if self._listener is not None:
+            # Disconnect from any further signals sent by the listener.
+            dispatcher.disconnect(dispatcher.Any, self._listener)
+            # Stop the listener.
             self._listener.stop()
             self._listener = None
 
     def teardown(self):
         self.disconnect()
+
+    def _handle_listener_data_received(self, data):
+        print("The SerialConnection heard: ", data)
+
+    def _handle_listener_read_error(self):
+        # Raise the alarm!
+        self.raise_alarm()
 
 
 
